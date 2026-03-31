8ebab33a2b640ef8525cdc984d0a9cb63f6293ff
aryaom74-max <aryaom74@gmail.com>
2026-03-31 22:19:33 +0700

    Update switch4.cpp

--- switch4.cpp
+++ switch4.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // Prosedur output
 void output() {
     cout << "\n=== HASIL SELEKSI ===" << endl;
-    cout << "Jurus Pilihan : " << namaJurusan << endl;
+    cout << "Jurusan Pilihan : " << namaJurusan << endl;
     cout << "Status Kuota    : " << statusKuota << endl;
     cout << "----------------------------------" << endl;
 }
