1cc3f12cc95a6687d20bd298727ef90718417bda
aryaom74-max <aryaom74@gmail.com>
2026-03-31 22:26:14 +0700

    Update switchALL.cpp

--- switchALL.cpp
+++ switchALL.cpp
@@ -10,7 +10,7 @@ string statusKuota;
 void input() {
     cout << "=== PENDAFTARAN JURUSAN KAMPUS ===" << endl;
     cout << "1. Teknologi Informasi" << endl;
-    cout << "2. Teknik Elektr" << endl;
+    cout << "2. Teknik Elektro" << endl;
     cout << "3. Sistem Inf" << endl;
     cout << "----------------------------------" << endl;
     cout << "Masukkan kode jurusan (1-3): ";
